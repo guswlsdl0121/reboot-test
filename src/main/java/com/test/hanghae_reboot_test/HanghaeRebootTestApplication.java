593a61a0b6ee4551618488e37dcb32c4910371d8
hyunjin <guswlsdl0121@gmail.com>
2024-09-09 11:33:46 +0900

    initialize

---
+++ src/main/java/com/test/hanghae_reboot_test/HanghaeRebootTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.hanghae_reboot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeRebootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeRebootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/hanghae_reboot_test/HanghaeRebootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.hanghae_reboot_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeRebootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
